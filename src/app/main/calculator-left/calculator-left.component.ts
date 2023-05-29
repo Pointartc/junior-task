a252397004f237c59a98efadeabb1154f349c495
Nazar <nazar.ternavskiy@gmail.com>
2023-05-29 21:09:34 +0300

    first-comit

---
+++ src/app/main/calculator-left/calculator-left.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator-left',
+  templateUrl: './calculator-left.component.html',
+  styleUrls: ['./calculator-left.component.scss']
+})
+export class CalculatorLeftComponent {
+
+}
