6524e619749770491323d9c0d537f77b064c9fdf
Pointartc <nazar.ternavskiy@gmail.com>
2023-05-31 23:06:21 +0300

    WIP mat-select

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -30,7 +32,10 @@ import { MatButtonModule } from '@angular/material/button';
     MatIconModule,
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/main/calculator-left/calculator-left.component.ts
+++ src/app/main/calculator-left/calculator-left.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {Currency} from '../../shared/interfaces/interfaces'
 
 
 @Component({
@@ -7,5 +7,28 @@ import { Component } from '@angular/core';
   templateUrl: './calculator-left.component.html',
   styleUrls: ['./calculator-left.component.scss']
 })
+
+
 export class CalculatorLeftComponent {
+  currency?: Currency[] = [
+    {value: 'TUSD',viewValue:'TUSD (Test US Dollar'},
+    {value: 'TEUR',viewValue:'TEUR (Test Euro)'},
+    {value: 'TCNY',viewValue:'TCNY (Test Chinese Yuan)'},
+    {value: 'TINR',viewValue:'TINR (Test Indian Rupee)'},
+    {value: 'TBRL',viewValue:'TBRL (Test Brazilian Real)'},
+    {value: 'TIDR',viewValue:'TIDR (Test Indonesian Rupiah)'},
+    {value: 'TUSD',viewValue:'TUSD (Test US Dollar)'},
+    {value: 'TEUR',viewValue:'TEUR (Test Euro)'},
+    {value: 'TCNY',viewValue:'TCNY (Test Chinese Yuan)'},
+    {value: 'TINR',viewValue:'TINR (Test Indian Rupee)'},
+    {value: 'TBRL',viewValue:'TBRL (Test Brazilian Real)'},
+    {value: 'TIDR',viewValue:'TIDR (Test Indonesian Rupiah)'},
+    {value: 'TUSD',viewValue:'TUSD (Test US Dollar)'},
+    {value: 'TEUR',viewValue:'TEUR (Test Euro)'},
+    {value: 'TCNY',viewValue:'TCNY (Test Chinese Yuan)'},
+    {value: 'TINR',viewValue:'TINR (Test Indian Rupee)'},
+    {value: 'TBRL',viewValue:'TBRL (Test Brazilian Real)'},
+    {value: 'TIDR',viewValue:'TIDR (Test Indonesian Rupiah)'},
+    {value: 'TUSD',viewValue:'TUSD (Test US Dollar)'}
+  ]
 }
